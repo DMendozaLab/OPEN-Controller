64d81caaa474042d2038cdaa0eb055aa9161a1f6
Landon Swartz <lgsm2n@mail.missouri.edu>
2019-07-30 12:48:27 -0500

    Filled out the plate class with properties and one methods.

--- MANDRAKEware/Experiment/ExperimentArrangement/ExperimentParts/Plate.cs
+++ MANDRAKEware/Experiment/ExperimentArrangement/ExperimentParts/Plate.cs
@@ -11,6 +11,63 @@ namespace MANDRAKEware.Experiment.ExperimentArrangement.ExperimentParts
     /// </summary>
     class Plate
     {
+        #region Properties
+        /// <summary>
+        /// x and y pos of plate
+        /// </summary>
+        private int xPos;
+        private int yPos;
+        /// <summary>
+        /// x and y offset of plate from tray
+        /// </summary>
+        private int xPlateOffset;
+        private int yPlateOffset;
+        /// <summary>
+        /// Is the entire plate active? All wells within active?
+        /// </summary>
+        private bool active; //only if whole plate is active
+        /// <summary>
+        /// Numbers of rows and columns in the plate
+        /// </summary>
+        private int numRows;
+        private int numCol;
+        /// <summary>
+        /// Array of Wells in the well
+        /// </summary>
+        private Well[,] wellArray;
 
+        public int YPos { get => yPos; set => yPos = value; }
+        public int XPos { get => xPos; set => xPos = value; }
+        public int XPlateOffset { get => xPlateOffset; set => xPlateOffset = value; }
+        public int YPlateOffset { get => yPlateOffset; set => yPlateOffset = value; }
+        public bool Active { get => active; set => active = value; }
+        public int NumRows { get => numRows; set => numRows = value; }
+        public int NumCol { get => numCol; set => numCol = value; }
+        public Well[,] WellArray { get => wellArray; set => wellArray = value; }
+        #endregion
+
+        #region Constructors
+        public Plate()
+        {
+            //TODO: do constructors
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Actives all the wells in a plate to true boolean statement
+        /// </summary>
+        public void ActivatePlate()
+        {
+            for(int i =0; i < NumRows; i++)
+            {
+                for(int j = 0; j <numCol; j++)
+                {
+                    WellArray[NumRows, NumCol].Active = true;
+                }
+            }
+        }
+
+        #endregion
     }
 }
