52d6ab674fb8ec7b0bef2b02e714a68e9cfe9d8f
Landon Swartz <lgsm2n@mail.missouri.edu>
2019-07-30 13:01:48 -0500

    Filled out tray class and added isActive() method to plate class

--- MANDRAKEware/Experiment/ExperimentArrangement/ExperimentParts/Tray.cs
+++ MANDRAKEware/Experiment/ExperimentArrangement/ExperimentParts/Tray.cs
@@ -11,6 +11,37 @@ namespace MANDRAKEware.Experiment.ExperimentArrangement.ExperimentParts
     /// </summary>
     class Tray
     {
+        #region Properties
+        private int xPos; //only one row of trays
+        private bool active; //whole tray is active
+        private Plate[,] plateArray;
+        /// <summary>
+        /// Numbers of rows and cols of plates in tray
+        /// </summary>
+        private int numRow;
+        private int numCol
 
+        public int XPos { get => xPos; set => xPos = value; }
+        public bool Active { get => active; set => active = value; }
+        internal Plate[,] PlateArray { get => plateArray; set => plateArray = value; }
+        public int NumRows { get => numRow; set => numRow = value; }
+        #endregion
+
+        #region Constructor
+        public Tray()
+        {
+            //TODO
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Activates all the plates in the tray
+        /// </summary>
+        public void ActivateTray()
+        {
+            for(int i = 0; )
+        }
+        #endregion
     }
 }
